6a1029c482cbdb1ecb2ac4f57eea8cf3410377ee
fiandev <fiandev@users.noreply.github.com>
2025-11-03 16:32:41 +0000

    deploy: ce57de829d3ee10b0b2d8234406b5c0dd9e3ae63

--- server/entry.mjs
+++ server/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_DV-qs-Na.mjs';
-import { manifest } from './manifest_B2mz1lc4.mjs';
+import { manifest } from './manifest_CSkDKbu9.mjs';
 
 const serverIslandMap = new Map();;
 
